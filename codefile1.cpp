02d79171cb8742df1750c87381552073d2873b55
mairohanhoon <rohantiwari2709@gmail.com>
2023-09-22 15:23:37 +0530

    new file

---
+++ codefile1.cpp
@@ -0,0 +1,16 @@
+sdasdadasdasd
+adsd
+andasd
+_ALIGNED_NEW_SUPPORTEDasd
+_ALIGNED_NEW_SUPPORTEDasd
+
+_ALIGNED_NEW_SUPPORTEDasd
+_ALIGNED_NEW_SUPPORTEDdg
+dfg
+dcv
+qwer
+transaction_safe_dynamicsdf
+while (std)
+{
+    /* code */
+}
